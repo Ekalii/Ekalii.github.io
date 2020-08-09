29008e30756a953c8d91fc601b75d032a151ccfe
ekalii <andoune0719@gmail.com>
2020-08-09 14:13:39 +0900

    first build upload

---
+++ precache-manifest.6d6c09cdfeb40d588445b5b3d8443c2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47b9fd59a7cfd6a28437b0a12e6d0bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90a8dd4fd5b65aecf3cf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b45aaff03ac9699c404",
+    "url": "/static/js/2.0d7d43cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d7d43cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a8dd4fd5b65aecf3cf",
+    "url": "/static/js/main.379843d8.chunk.js"
+  },
+  {
+    "revision": "13ff3f6c52d527a3b5c1",
+    "url": "/static/js/runtime-main.cf4e4ba0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
