7d9d183c0862b82abdaefd4986169c662f9cc9e7
ekalii <andoune0719@gmail.com>
2020-08-30 14:11:13 +0900

    portfolio objects

---
+++ src/portfolio.tsx
@@ -0,0 +1,28 @@
+export const greeting = {
+	username: 'Ekalii',
+	title: 'Hello I\'m Ekalii!'
+};
+
+export const socialMediaLinks = {
+	github: 'https://github.com/ekalii',
+	linkedin: 'https://www.linkedin.com/in/doeon-an-2b8b21164',
+	gmail: 'andoune0719@gmail.com',
+	facebook: 'https://www.facebook.com/profile.php?id=100005762592592',
+	medium: 'https://medium.com/@andoune0719'
+};
+
+export const skills = {
+
+};
+
+export const openSourceProjects = {
+
+};
+
+export const achievements = {
+
+};
+
+export const blogs = {
+
+};
