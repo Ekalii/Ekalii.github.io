f885c54be7ff89e15b8541b7275453c0023361cc
An Do Eon <andoune0719@gmail.com>
2020-09-11 22:27:28 +0900

    add skills:

--- src/portfolio.tsx
+++ src/portfolio.tsx
@@ -3,7 +3,7 @@ import emoji from 'react-easy-emoji';
 export const greeting = {
 	username: 'Ekalii',
 	title: 'Hello I\'m Ekalii!',
-	subTitle: emoji("Majoring in Software👨‍🎓 | Wanna be Full-Stack Developer👨‍💻 | Working hard, fully-passionated! | Future Google Developer")
+	subTitle: emoji("Majoring in Software👨‍🎓 | Wanna be Full-Stack Developer👨‍💻 | Working Hard, Fully Passionated! | Future Google Developer")
 };
 
 export const socialMediaLinks = {
@@ -15,17 +15,27 @@ export const socialMediaLinks = {
 };
 
 export const skills = {
-
+	subTitle: "FRONT-END DEVELOPER WHO WANTS TO EXPLORE MORE TECHs AND BACK-END",
+	describeSkills: [
+		emoji("✔ Develop reponsive web application"),
+		emoji("✔ Interesting in PWA(Progressive Web App) and SPA"),
+		emoji("✔ Integration of third party services such as AWS, Firebase"),
+		emoji("✔ Proficient in using Git and cooperating with coworker"),
+	] 
 };
 
 export const openSourceProjects = {
-
+	subTitle: "Nothing yet..."
 };
+
+export const toyProjects = {
+	subTitle: ""
+}
+
+export const experience= {
+
+}
 
 export const achievements = {
-
-};
-
-export const blogs = {
 
 };
