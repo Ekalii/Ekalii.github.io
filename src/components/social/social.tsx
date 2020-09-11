abc038c709ca379d7417363761f3b41a91999de3
An Do Eon <andoune0719@gmail.com>
2020-09-11 12:00:08 +0900

    make social component: icons and link

--- src/components/social/social.tsx
+++ src/components/social/social.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import './social.css'
+import { socialMediaLinks } from "../../portfolio";
 
 function Social() {
 	return(
 		<div className="social-media-links">
-			ADD Social
+			<a href={socialMediaLinks.github} className="icon-button github" target="blank">
+				<i className="fab fa-github"></i>
+			</a>
+			<a href={socialMediaLinks.linkedin} className="icon-button linkedin" target="blank">
+				<i className="fab fa-linkedin"></i>
+			</a>
+			<a href={`mailto:${socialMediaLinks.gmail}`} className="icon-button google" target="blank">
+				<i className="fab fa-google"></i>
+			</a>
+			<a href={socialMediaLinks.facebook} className="icon-button facebook" target="blank">
+				<i className="fab fa-facebook"></i>
+			</a>
+			<a href={socialMediaLinks.medium} className="icon-button medium" target="blank">
+				<i className="fab fa-medium"></i>
+			</a>
 		</div>
 	);
 }
