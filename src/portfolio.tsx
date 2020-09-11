36ff5ad553f5125eb223be437f8c3f6d11a4146a
An Do Eon <andoune0719@gmail.com>
2020-09-11 12:00:59 +0900

    change facebook link and subTitle

--- src/portfolio.tsx
+++ src/portfolio.tsx
@@ -1,13 +1,16 @@
+import emoji from 'react-easy-emoji';
+
 export const greeting = {
 	username: 'Ekalii',
-	title: 'Hello I\'m Ekalii!'
+	title: 'Hello I\'m Ekalii!',
+	subTitle: emoji("Majoring in Software👨‍🎓 | Wanna be Full-Stack Developer👨‍💻 | Working hard, fully-passionated! | Future Google Developer")
 };
 
 export const socialMediaLinks = {
 	github: 'https://github.com/ekalii',
 	linkedin: 'https://www.linkedin.com/in/doeon-an-2b8b21164',
 	gmail: 'andoune0719@gmail.com',
-	facebook: 'https://www.facebook.com/profile.php?id=100005762592592',
+	facebook: 'https://www.facebook.com/andoeon/',
 	medium: 'https://medium.com/@andoune0719'
 };
 
