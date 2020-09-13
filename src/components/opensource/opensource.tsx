fa65e2e35d4a394cb5d28a2a9e7b2954231d3f43
An Do Eon <andoune0719@gmail.com>
2020-09-13 22:17:47 +0900

    fix error

--- src/components/opensource/opensource.tsx
+++ src/components/opensource/opensource.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, lazy, Suspense } from 'react';
 import ApolloClient, { gql } from 'apollo-boost';
 import './opensource.css';
 import GithubCard from '../githubcard/githubcard';
-import { achievements } from '../../portfolio';
+import { projects } from '../../portfolio';
 
 function OpenSource() {
     // const RepoCard = lazy(() => import('../../components/repocard/repocard'))
@@ -71,7 +71,7 @@ function OpenSource() {
     }
     return (
         <div className="main" id="opensource">
-            <h1 className="project-title">{achievements.title}</h1>
+            <h1 className="project-title">{projects.title}</h1>
             <div className="repo-cards">
                 {repos.map((v, i) => {
                     return <GithubCard repos={v} />;
