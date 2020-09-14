7cab2efefdbd455358e633ae029853e17c616144
An Do Eon <andoune0719@gmail.com>
2020-09-14 20:31:31 +0900

    contact is finished...wow

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './contact.css';
+import { contactInfo } from '../../portfolio';
+import Social from '../social/social';
 
 function Contact() {
     return (
         <section className="section--contact">
+            <div className="inner">
 
+                <div className="wrapper">
+
+                    <div className="contact-title">
+                        <p>{contactInfo.title}</p>
+                    </div>
+                    <div className="contact-subtitle">
+                        <p>{contactInfo.subTitle}</p>
+                    </div>
+                    <div className="contact-introduce">
+                        <p>{contactInfo.introduce}</p>
+                    </div>
+                    <div className="contact-content">
+                        <p>I'm Open To Opportunities! 😁</p>
+                    </div>
+                    <Social />
+
+                </div>
+                <div className="img-ikalli">
+                    <img src={require("../../assets/Ikalli.jpg")} alt="happy Ikalli" />
+                </div>
+                
+            </div>
         </section>
     );
 }
