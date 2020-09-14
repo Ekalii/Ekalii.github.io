52d8bf6af28d98897d38327174b17f2117565769
An Do Eon <andoune0719@gmail.com>
2020-09-14 18:04:24 +0900

    create blog card

---
+++ src/components/blogcard/blogcard.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './blogcard.css';
+
+function BlogCard({ blogs }: any) {
+    const Fade: any = require('react-reveal/Fade')
+    return (
+        <Fade bottom duration={800}>
+        <a className="blog-card" href={blogs.url}>
+            <div className="blog-card-title">
+                <p>{blogs.title}</p>
+            </div>
+            <div className="blog-card-desc">
+                <p>{blogs.subTitle}</p>
+            </div>
+        </a>
+        </Fade>
+    )
+}
+export default BlogCard;
