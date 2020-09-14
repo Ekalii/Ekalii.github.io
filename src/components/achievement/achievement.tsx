1a0f68ea2e5fcf1d031c8c8b149d23607e0ea8f8
An Do Eon <andoune0719@gmail.com>
2020-09-14 11:51:56 +0900

    css problem fix and achievementCard component

--- src/components/achievement/achievement.tsx
+++ src/components/achievement/achievement.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import './achievement.css';
 import { achievements } from '../../portfolio';
+import AchievementCard from '../ahievementcard/achievementcard'
 
 function Achievement() {
     return (
         <section className="section--achievement" id="#achievement">
             <div className="inner">
 
-                <div className="title">
+                <div className="achieve-title">
                     <p>{achievements.title}</p>
                 </div>
 
-                <div className="sub-title">
+                <div className="achieve-sub-title">
                     <p>{achievements.subTitle}</p>
+                </div>
+
+                <div className="achieve-content">
+                    {achievements.list.map(v => {
+                        return <AchievementCard achieves={v} />
+                    })}
                 </div>
 
             </div>
