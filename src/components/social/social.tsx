56189b20b13db79440206328d4900c9c59c40221
An Do Eon <andoune0719@gmail.com>
2020-09-16 21:48:10 +0900

    value typing error fix, add rel property

--- src/components/social/social.tsx
+++ src/components/social/social.tsx
@@ -5,19 +5,19 @@ import { socialMediaLinks } from "../../portfolio";
 function Social() {
 	return(
 		<div className="social-media-links">
-			<a href={socialMediaLinks.github} className="icon-button github" target="blank">
+			<a href={socialMediaLinks.github} className="icon-button github" target="_blank" rel="noopener noreferrer">
 				<i className="fab fa-github"></i>
 			</a>
-			<a href={socialMediaLinks.linkedin} className="icon-button linkedin" target="blank">
+			<a href={socialMediaLinks.linkedin} className="icon-button linkedin" target="_blank" rel="noopener noreferrer">
 				<i className="fab fa-linkedin"></i>
 			</a>
-			<a href={`mailto:${socialMediaLinks.gmail}`} className="icon-button google" target="blank">
+			<a href={`mailto:${socialMediaLinks.gmail}`} className="icon-button google" target="_blank" rel="noopener noreferrer">
 				<i className="fab fa-google"></i>
 			</a>
-			<a href={socialMediaLinks.facebook} className="icon-button facebook" target="blank">
+			<a href={socialMediaLinks.facebook} className="icon-button facebook" target="_blank" rel="noopener noreferrer">
 				<i className="fab fa-facebook"></i>
 			</a>
-			<a href={socialMediaLinks.medium} className="icon-button medium" target="blank">
+			<a href={socialMediaLinks.medium} className="icon-button medium" target="_blank" rel="noopener noreferrer">
 				<i className="fab fa-medium"></i>
 			</a>
 		</div>
