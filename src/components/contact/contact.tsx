162ef039388f6ba9b49602f775be7e2969ce1477
An Do Eon <andoune0719@gmail.com>
2020-09-22 09:20:01 +0900

    delete "photo" in alt

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -15,7 +15,7 @@ function Contact() {
             <div className="inner">
                 
                 <div className="profile-img">
-                    <img src={require("../../assets/Ikalli.jpg")} alt="profile photo" />
+                    <img src={require("../../assets/Ikalli.jpg")} alt="profile" />
                 </div>
                 <div className="wrapper-inner">
                     <div className="contact-subtitle">
