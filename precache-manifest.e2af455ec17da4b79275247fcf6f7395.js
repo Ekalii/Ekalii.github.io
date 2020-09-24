f4a84075a84bf52759b0c283c74eb03d66d60f4b
An Do Eon <andoune0719@gmail.com>
2020-09-24 21:52:23 +0900

    Updates

---
+++ precache-manifest.e2af455ec17da4b79275247fcf6f7395.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016747ec6f18efedffd179e15d704c8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "739c1f791fb75ddf2b7b",
+    "url": "/static/css/main.eab916d1.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "739c1f791fb75ddf2b7b",
+    "url": "/static/js/main.2ac3ff49.chunk.js"
+  },
+  {
+    "revision": "13ff3f6c52d527a3b5c1",
+    "url": "/static/js/runtime-main.cf4e4ba0.js"
+  },
+  {
+    "revision": "247f04ab9e6a899e9b12a37036f0dc9c",
+    "url": "/static/media/Ikalli.247f04ab.jpg"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/static/media/sad-icon.b88924d5.png"
+  }
+]);
